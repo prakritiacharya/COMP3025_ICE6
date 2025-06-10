098e4b22f29e47146b0af44a0d1eac82f43b1866
Prakriti Acharya <prakriti@acharya.com>
2025-06-10 15:36:39 -0400

    Fixed the bug for view binding

--- app/src/main/java/ca/georgiancollege/ice6/OptionsActivity.kt
+++ app/src/main/java/ca/georgiancollege/ice6/OptionsActivity.kt
@@ -5,18 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ca.georgiancollege.ice6.databinding.ActivityOptionsBinding
 
 class OptionsActivity : AppCompatActivity()
 {
+   private lateinit var binding: ActivityOptionsBinding
    override fun onCreate(savedInstanceState: Bundle?)
    {
       super.onCreate(savedInstanceState)
+      binding = ActivityOptionsBinding.inflate(layoutInflater)
       enableEdgeToEdge()
-      setContentView(R.layout.activity_options)
-      ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+      setContentView(binding.root)
+      ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
          val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
          v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
          insets
       }
    }
 }
+
