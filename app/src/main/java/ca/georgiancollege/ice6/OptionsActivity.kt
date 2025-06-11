99a9f2336911c3eb7a1141e2e1bcb9f8a6439c1f
Prakriti Acharya <prakriti@acharya.com>
2025-06-11 10:09:40 -0400

    Wired up the ToggleButton in  OptionsActivity.kt

--- app/src/main/java/ca/georgiancollege/ice6/OptionsActivity.kt
+++ app/src/main/java/ca/georgiancollege/ice6/OptionsActivity.kt
@@ -1,6 +1,7 @@
 package ca.georgiancollege.ice6
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,6 +21,16 @@ class OptionsActivity : AppCompatActivity()
          val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
          v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
          insets
+      }
+      binding.ThemeToggleButton.setOnCheckedChangeListener {_, isChecked ->
+         if (isChecked)
+         {
+            Toast.makeText(this, "Dark Theme Selected", Toast.LENGTH_SHORT).show()
+         }
+         else
+         {
+            Toast.makeText(this, "Light Theme Selected", Toast.LENGTH_SHORT).show()
+         }
       }
    }
 }
